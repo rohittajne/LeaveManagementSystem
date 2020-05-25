77d5efe853f9ecf18c3266a121bef384d823e27a
Rohit Tajne <rohit.tajne@gmail.com>
2020-05-25 17:07:51 +0530

    Basics
    Basics

---
+++ src/main/java/com/LeaveMSys/leavemsys/LeavemsysApplication.java
@@ -0,0 +1,13 @@
+package com.LeaveMSys.leavemsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavemsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavemsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LeaveMSys/leavemsys/LeavemsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LeaveMSys.leavemsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavemsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
